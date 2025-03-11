6a23e85cdbf133a7c7b3d190f6f948f808798fff
Moritisimor <moritusdiscord@gmail.com>
2025-03-11 14:00:43 +0100

    Delete Python/PythonSkripte directory

--- Python/PythonSkripte/PyStat.py
+++
@@ -1,7 +0,0 @@
-#Willkommensnachricht und Import von Math
-
-print("Willkommen zum Simple PyStat, Einem Statistik Tool")
-Liste = (input("Geben Sie Ihre Liste an Daten an (x, y, z): "))
-Liste = [float(num) for num in Liste.split()]
-ArithMittel = sum(Liste) / len(Liste)
-print(ArithMittel)
