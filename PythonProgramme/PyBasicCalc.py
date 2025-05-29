c9adc7f031ac810bcd9fd1710452a1e2f298612f
Moritisimor <moritzgierschner@protonmail.com>
2025-05-29 17:10:07 +0200

    put an extra ": " at the end of the operator input

--- PythonProgramme/PyBasicCalc.py
+++ PythonProgramme/PyBasicCalc.py
@@ -1,7 +1,7 @@
 print("Welcome to the Basic Calculator written in python.")
 x = float(input("Enter your first number: "))
 y = float(input("Enter your second number: "))
-op = input("Enter your operator")
+op = input("Enter your operator: ")
 if op == "+":
     print(x + y)
 elif op == "-":
