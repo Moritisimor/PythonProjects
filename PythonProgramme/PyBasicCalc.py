7d543a8acf664725ee758a5a9b46bfbc4cad96ce
Moritisimor <moritzgierschner@protonmail.com>
2025-05-29 17:13:19 +0200

    Added Error Handling

--- PythonProgramme/PyBasicCalc.py
+++ PythonProgramme/PyBasicCalc.py
@@ -1,12 +1,15 @@
 print("Welcome to the Basic Calculator written in python.")
-x = float(input("Enter your first number: "))
-y = float(input("Enter your second number: "))
-op = input("Enter your operator: ")
-if op == "+":
-    print(x + y)
-elif op == "-":
-    print(x - y)
-elif op == "*":
-    print(x * y)
-elif op == "/":
-    print(x / y)
+try:
+    x = float(input("Enter your first number: "))
+    y = float(input("Enter your second number: "))
+    op = input("Enter your operator: ")
+    if op == "+":
+        print(x + y)
+    elif op == "-":
+        print(x - y)
+    elif op == "*":
+        print(x * y)
+    elif op == "/":
+        print(x / y)
+except ValueError:
+    print("Please only enter numbers.")
