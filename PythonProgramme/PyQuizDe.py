c52aac99c8d04ad1c1b53433af427920f613f9c3
Moritisimor <moritzgierschner@protonmail.com>
2025-06-07 16:31:52 +0200

    Made the code more pythonic (lowercase function names and parameters)

--- PythonProgramme/PyQuizDe.py
+++ PythonProgramme/PyQuizDe.py
@@ -3,11 +3,11 @@ from random import randint
 print("Willkommen zum Quiz")
 input("Bitte drücken Sie Enter um fortzufahren: ")
 
-def Quiz(frage, richtigeAntwort):
+def quiz(frage, richtigeantwort):
     while True:
         print(frage)
         antwort = input("Geben Sie Ihre Antwort ein: ")
-        if antwort.lower() in [richtigeAntwort]:
+        if antwort.lower() in [richtigeantwort]:
             print("Das ist korrekt!")
             break
         else:
@@ -18,34 +18,34 @@ while True:
     QuizAuswahl = randint (0,9)
 
     if QuizAuswahl == 0:
-        Quiz("Was ist das größte Tier der Welt?", "blauwal")
+        quiz("Was ist das größte Tier der Welt?", "blauwal")
         
     elif QuizAuswahl == 1:
-        Quiz("In welchem Land liegt der Kilimandscharo?", "tansania")
+        quiz("In welchem Land liegt der Kilimandscharo?", "tansania")
 
     elif QuizAuswahl == 2:
-        Quiz("Was ist der kleinste Staat der Welt?", "vatikan") 
+        quiz("Was ist der kleinste Staat der Welt?", "vatikan")
     
     elif QuizAuswahl == 3:
-        Quiz("In welchem Jahr begann der zweite Weltkrieg", "1939")
+        quiz("In welchem Jahr begann der zweite Weltkrieg", "1939")
 
     elif QuizAuswahl == 4:
-        Quiz("Wer war der erste Mensch im Weltraum?", "juri gagarin")
+        quiz("Wer war der erste Mensch im Weltraum?", "juri gagarin")
 
     elif QuizAuswahl == 5:
-        Quiz("Wie viele Sterne hat die Flagge der USA?", "50")
+        quiz("Wie viele Sterne hat die Flagge der USA?", "50")
 
     elif QuizAuswahl == 6:
-        Quiz("Was ist das schnellste Tier an Land?", "gepard")
+        quiz("Was ist das schnellste Tier an Land?", "gepard")
 
     elif QuizAuswahl == 7:
-        Quiz("Was war das erste Videospiel der Welt?", "pong")
+        quiz("Was war das erste Videospiel der Welt?", "pong")
 
     elif QuizAuswahl == 8:
-        Quiz("Wie viele Planeten hat unser Sonnensystem?", "8")
+        quiz("Wie viele Planeten hat unser Sonnensystem?", "8")
 
     elif QuizAuswahl == 9:
-        Quiz("Wer ist der Autor von dem Roman 1984?", "george orwell")
+        quiz("Wer ist der Autor von dem Roman 1984?", "george orwell")
 
     nochMal = input("Möchten Sie noch mal spielen? ")
     if nochMal in ["nein", "n"]:
