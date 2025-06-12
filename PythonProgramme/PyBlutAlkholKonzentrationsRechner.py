1bc00c037df793c6639cd63d546b4d77bbc1c2fd
Moritisimor <devMoritisimor@proton.me>
2025-06-12 20:37:28 +0200

    Added error handling and more comments

--- PythonProgramme/PyBlutAlkholKonzentrationsRechner.py
+++ PythonProgramme/PyBlutAlkholKonzentrationsRechner.py
@@ -1,19 +1,23 @@
-from time import sleep
-
-print("Willkommen zum Blutalkoholkonzentrationsrechner.") #Willkommensnachricht
-AnzahlBier = float(input("Wie viele Flaschen oder Gläser Bier haben Sie getrunken? (0,5L): ")) #Eingabe Anzahl Bier
-AnzahlKorn = float(input("Wie viele Flaschen Korn haben Sie getrunken? (0,02L): ")) #Eingabe Anzahl Korn
-Gewicht = float(input("Geben Sie Ihr Gewicht in KG an: ")) #Eingabe Gewicht
-while True: #Da Geschlecht ein String ist, kann es potenziell undefiniert sein oder die Eingabe nicht im Array liegen
-    Geschlecht = input("Sind Sie männlich oder weiblich?: ") #Eingabe Geschlecht
-    if Geschlecht.lower() in["männlich","m","weiblich","w"]:
+print("Willkommen zum Blutalkoholkonzentrationsrechner.") # Willkommensnachricht
+while True:
+    try:
+        AnzahlBier = float(input("Wie viele Flaschen oder Gläser Bier haben Sie getrunken? (0,5L): ")) # Eingabe Bier
+        AnzahlKorn = float(input("Wie viele Flaschen Korn haben Sie getrunken? (0,02L): ")) # Eingabe Korn
+        Gewicht = float(input("Geben Sie Ihr Gewicht in KG an: ")) # Eingabe Gewicht#
         break
-    else: #Deswegen werden Fehler hier abgefangen bei ungültiger Eingabe
-        print("Bitte wählen Sie ein gültiges Geschlecht aus.")
+    except ValueError:
+        print("Bitte geben Sie nur Zahlen an.", "\n", "Fange neu an...")
 
-if Geschlecht.lower() in["männlich","m"]:
+while True: # Wenn der folgende Input nicht männlich oder weiblich entspricht geht es hier wieder zurück
+    Geschlecht = input("Sind Sie männlich oder weiblich?: ") # Eingabe Geschlecht
+    if Geschlecht.lower() in["männlich","m","weiblich","w"]: # Prüfung ob die Eingabe einer der gültigen Einträge entspricht
+        break # Wenn das so ist, brich die Schleife ab.
+    else: # Sonst
+        print("Bitte wählen Sie ein gültiges Geschlecht aus.") # Sag dem Nutzer, dass seine Eingabe ungültig war und geh zurück zum Anfang der While True Schleife
+
+if Geschlecht.lower() in["männlich","m"]: # Wenn die Eingabe männlich ist:
     GeschlechtFaktor = 0.68 #Definierung des Geschlechtfaktors bei Männlichkeit
-else:
+else: # Sonst:
     GeschlechtFaktor = 0.55 #Definierung des Geschlechtfaktors bei Weiblichkeit
 
 #Berechnungen
@@ -21,18 +25,15 @@ else:
 GramAlkoholBier = 18 * AnzahlBier
 GramAlkoholKorn = 5 * AnzahlKorn
 GeschlechtGewichtBilanz = GeschlechtFaktor * Gewicht
-if AnzahlBier == 0:
+if AnzahlBier == 0: # Wenn der Nutzer kein Bier getrunken hat, fällt das nicht in die Berechnung, denn durch Null teilen wird nichts bringen
     BAK = (GramAlkoholKorn / GeschlechtGewichtBilanz)
-elif AnzahlKorn == 0:
+elif AnzahlKorn == 0: # Gleiches Prinzip, nur mit Korn
     BAK = (GramAlkoholBier / GeschlechtGewichtBilanz)
-else:
+else: # Wenn beide größer als Null 
     BAK = ((GramAlkoholBier + GramAlkoholKorn) / GeschlechtGewichtBilanz)
 
-print("Ihre geschätzte Blutalkoholkonzentration liegt bei:",BAK) #Ergebnis
-sleep(2)
+print("Ihre geschätzte Blutalkoholkonzentration liegt bei:", BAK) #Ergebnis
 if BAK > 1.09:
     print("Ihre BAK ist deutlich zu hoch, Autofahren sehr illegal! Straftat mit bis zu 5 Jahre Führerscheinentzug und möglicher Freiheitsstrafe.")
-    sleep(10)
 elif BAK > 0.49:
     print("Ihre BAK ist zu hoch, Autofahren illegal! Ordnungswidrigkeit mit bis zu 1000€ Strafe.")
-    sleep(10)
