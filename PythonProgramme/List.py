ec568df000b85f4cfe8e75881c58b88b3bd0affe
Moritisimor <devMoritisimor@proton.me>
2025-06-21 13:31:14 +0200

    Put welcome message outside of loop, made list work with floats, added several ways to end the list and error handling if list is empty

--- PythonProgramme/List.py
+++ PythonProgramme/List.py
@@ -1,29 +1,33 @@
+print("Welcome to List.py! This program will allow you to sort numerical lists and calculate the average number.")
+
 while True:
-    print("Welcome to List.py! This program will allow you to sort numerical lists and calculate the average number.")
-    print("Enter each number individually and end the list by typing 'end' at your last input.")
+    print("Enter each number individually and end the list by typing 'end' at your last input or by just leaving it blank.")
 
     listToBeSorted = []
 
     while True:
         try:
             entry = (input("Enter your number: "))
-            if entry.lower() == "end":
+            if entry.lower().strip() in ["end", ""]:
                 break
             else:
-                listToBeSorted.append(int(entry))
+                listToBeSorted.append(float(entry))
         except ValueError:
             print("Only enter numbers")
 
-    print("\nYour original list:")
-    print(listToBeSorted)
+    if len(listToBeSorted) == 0:
+        print("Your list is empty.")
+    else:
+        print("\nYour original list:")
+        print(listToBeSorted)
 
-    print("\nYour sorted list:")
-    print(sorted(listToBeSorted))
+        print("\nYour sorted list:")
+        print(sorted(listToBeSorted))
 
-    print("\nThe average number in your list:")
-    print(sum(listToBeSorted) / len(listToBeSorted))
+        print("\nThe average number in your list:")
+        print(sum(listToBeSorted) / len(listToBeSorted))
 
-    again = input("Would you like to sort another list? \nY/N: ")
+    again = input("\nWould you like to sort another list? \nY/N: ")
     if again.lower() in ["y", "yes"]:
         continue
     else:
