61459be7848c1627e97e3a106104bb67d928ff0c
Moritisimor <devMoritisimor@proton.me>
2025-07-15 15:37:13 +0200

    Creating several accounts is now possible

--- PythonPrograms/TKBankApp.py
+++ PythonPrograms/TKBankApp.py
@@ -1,7 +1,8 @@
 import tkinter as tk
+from tkinter import messagebox
 
 class User:
-    def __init__(self, name, password, money):
+    def __init__(self, name, password, money = 0):
         self.name = name
         self.password = password
         self.money = money
@@ -12,22 +13,24 @@ class User:
         else:
             return None
 
-userList = [
-user1 := User(None, None, 0)
-]
+userList = []
 
 loggedIn = None
 
-def setuser(user: User, name, password):
-    user.name = name
-    user.password = password
-
-def login(name, password):
-    global loggedIn
-    for user in userList:
-        loggedIn = user.authlogin(name, password)
-
 def startregwindow():
+    def register(name, password):
+        for user in userList:
+            if user.name == name:
+                messagebox.showerror("Error", "User with this name already exists.", parent=regwindow)
+                return
+
+        if rwuserentry == "" or rwpassentry == "":
+            messagebox.showerror("Error", "Input fields can not be blank.", parent=regwindow)
+        else:
+            userList.append(User(name, password))
+            messagebox.showinfo("Success", "Succesfully created account!", parent=regwindow)
+            regwindow.destroy()
+
     regwindow = tk.Toplevel()
     regwindow.geometry("200x250")
     regwindow.title("Registration Window")
@@ -47,12 +50,25 @@ def startregwindow():
     rwpassentry = tk.Entry(regwindow)
     rwpassentry.pack(pady=5)
 
-    rwsubmitbutton = tk.Button(regwindow, text="Submit", command=lambda: setuser(user1, rwuserentry.get(), rwpassentry.get()))
+    rwsubmitbutton = tk.Button(regwindow, text="Submit", command=lambda: register(rwuserentry.get(), rwpassentry.get()))
     rwsubmitbutton.pack(pady=5)
 
 def startloginwindow():
+    def login(name, password):
+        global loggedIn
+        for user in userList:
+            user.authlogin(name, password)
+            if user.authlogin(name, password) is not None:
+                loggedIn = user.authlogin(name, password)
+                break
+        if loggedIn is None:
+            messagebox.showerror("Error", "Login failed.", parent=loginwindow)
+        else:
+            messagebox.showinfo("Success", f"Succesfully logged in as {name}", parent=loginwindow)
+            loginwindow.destroy()
+
     loginwindow = tk.Toplevel()
-    loginwindow.geometry("200x250")
+    loginwindow.geometry("225x250")
     loginwindow.title("Login Window")
 
     lwwelcomelabel = tk.Label(loginwindow, text="Log into an existing account here")
@@ -78,10 +94,7 @@ mainWindow.geometry("375x500")
 mainWindow.title("Main Menu")
 
 mwWelcomeLabel = tk.Label(mainWindow, text="Welcome to my bank app!")
-mwWelcomeLabel.pack(pady=15)
-
-mwRefreshButton = tk.Button(mainWindow, text="Refresh", command=lambda: mwLoginStatusLabel.config(text=f"Logged in as {loggedIn.name}"))
-mwRefreshButton.pack(pady=5)
+mwWelcomeLabel.pack(pady=10)
 
 mwLoginStatusLabel = tk.Label(mainWindow, text="")
 mwLoginStatusLabel.pack(pady=2)
@@ -93,3 +106,5 @@ logInButton = tk.Button(mainWindow, text="Log in", command=lambda: startloginwin
 logInButton.pack(pady=20)
 
 mainWindow.mainloop()
+
+# I do not recommend using tkinter for stuff like this.
