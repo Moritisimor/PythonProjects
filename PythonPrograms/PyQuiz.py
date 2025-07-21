5a7a6b3d5da9f0b921be6b29543f8d1baaf2a85f
Moritisimor <devMoritisimor@proton.me>
2025-07-21 21:50:10 +0200

    Made PyQuiz a bit more modular, and it is now easier to add new quizzes

--- PythonPrograms/PyQuiz.py
+++ PythonPrograms/PyQuiz.py
@@ -1,7 +1,10 @@
 from random import randint
 
 print("Welcome to PyQuiz!")
-input("Press enter to continue: ")
+print("You may exit the quiz at any time by typing 'exit'")
+cont = input("Press enter to start the quiz: ")
+if cont.lower() in ["exit", "quit"]:
+    exit()
 
 def quiz(question, correctanswer):
     while True:
@@ -9,43 +12,25 @@ def quiz(question, correctanswer):
         answer = input("Enter your answer: ")
         if answer.lower() == correctanswer:
             print("That is correct!")
+        elif answer.lower() == "exit":
+            exit()
         else:
             print("That is wrong!")
         break
 
 while True:
-    quizSelection = randint(0,9)
+    quizlist = [ # Lambda is required to pass the function itself, not its return value (which is None)
+        lambda: quiz("What is the largest animal in the world?", "blue whale"),
+        lambda: quiz("In which country is Mount Kilimanjaro located?", "tanzania"),
+        lambda: quiz("What is the smallest country in the world?", "vatican"),
+        lambda: quiz("In which year did World War II begin?", "1939"),
+        lambda: quiz("Who was the first human in space?", "yuri gagarin"),
+        lambda: quiz("How many stars are on the US flag?", "50"),
+        lambda: quiz("What is the fastest land animal?", "cheetah"),
+        lambda: quiz("What was the world's first video game?", "pong"),
+        lambda: quiz("How many planets are in our solar system?", "8"),
+        lambda: quiz("Who is the author of the novel 1984?", "george orwell"),
+    ]
 
-    if quizSelection == 0:
-        quiz("What is the largest animal in the world?", "blue whale")
-        
-    elif quizSelection == 1:
-        quiz("In which country is Mount Kilimanjaro located?", "tanzania")
-
-    elif quizSelection == 2:
-        quiz("What is the smallest country in the world?", "vatican")
-    
-    elif quizSelection == 3:
-        quiz("In which year did World War II begin?", "1939")
-
-    elif quizSelection == 4:
-        quiz("Who was the first human in space?", "yuri gagarin")
-
-    elif quizSelection == 5:
-        quiz("How many stars are on the US flag?", "50")
-
-    elif quizSelection == 6:
-        quiz("What is the fastest land animal?", "cheetah")
-
-    elif quizSelection == 7:
-        quiz("What was the world's first video game?", "pong")
-
-    elif quizSelection == 8:
-        quiz("How many planets are in our solar system?", "8")
-
-    elif quizSelection == 9:
-        quiz("Who is the author of the novel 1984?", "george orwell")
-
-    playAgain = input("Would you like to play again? ")
-    if playAgain in ["no", "n"]:
-        break
+    quizSelection = randint(0, len(quizlist))
+    quizlist[quizSelection]()
